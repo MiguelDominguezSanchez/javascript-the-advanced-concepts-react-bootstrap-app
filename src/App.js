b638ea7e94a21a41c21c4b0ec31c20e32ab26706
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-30 09:21:01 +0200

    create-react-app react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import Alert1 from './components/Alert'
+import MenuNavBar from './components/MenuNavbar/MenuNavbar'
+
+function App() {
+	return (
+		<div className='App'>
+			<MenuNavBar />
+		</div>
+	)
+}
+
+export default App
