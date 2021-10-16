044c53b55ebfdf439bd7e6bdd8bac93d4e884e7d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-10-16 07:21:51 +0200

    exercise

--- src/exercises.js
+++ src/exercises.js
@@ -160,6 +160,7 @@ console.log(fiona === ogre)
 // console.log(ogre.attack())
 */
 
+/*
 class Character {
 	constructor(name, weapon) {
 		this.name = name
@@ -194,3 +195,118 @@ console.log(dolby.attack())
 const shrek = new Ogre('Shrek', 'club', 'green')
 console.log(shrek)
 console.log(shrek.makeFort())
+*/
+
+/*
+class MyArray {
+	constructor() {
+		this.length = 0
+		this.data = {}
+	}
+
+	get(index) {
+		return this.data[index]
+	}
+
+	push(item) {
+		this.data[this.length] = item
+		this.length++
+		return this.length
+	}
+
+	pop() {
+		const lastItem = this.data[this.length - 1]
+		delete this.data[this.length - 1]
+		this.length--
+		return lastItem
+	}
+
+	delete(index) {
+		const item = this.data[index]
+		this.shiftItems(index)
+		// return lastItem
+	}
+
+	shiftItems(index) {
+		for (let i = index; i < this.length - 1; i++) {
+			this.data[i] = this.data[i + 1]
+		}
+		delete this.data[this.length - 1]
+		this.length--
+	}
+}
+
+const newArray = new MyArray()
+newArray.push('hi')
+newArray.push('you')
+newArray.push('!')
+// console.log(newArray.get(0))
+// newArray.pop()
+// newArray.pop()
+// newArray.delete(1)
+newArray.delete(0)
+newArray.push('are')
+newArray.push('nice')
+newArray.delete(1)
+console.log(newArray)
+*/
+/*
+function reverse(str) {
+	// check input
+	if (!str || str.length < 2 || typeof str !== 'string') {
+		return 'hmm that it not good'
+	}
+
+	const backwards = []
+	const totalItems = str.length - 1
+	for (let i = totalItems; i >= 0; i--) {
+		backwards.push(str[i])
+	}
+	console.log(backwards)
+
+	return backwards.join('')
+}
+
+function reverse2(str) {
+	return str.split('').reverse().join('')
+}
+
+// const reverse3 = (str) => [str].split('').reverse().join('')
+const reverse3 = (str) => [...str].reverse().join('')
+
+// reverse('Hi My name is Andrei')
+console.log(reverse2('Hi My name is Andrei'))
+console.log(reverse3('Hi My name is Andrei'))
+*/
+
+function mergeSortedArrays(array1, array2) {
+	const mergedArray = []
+	let array1Item = array1[0]
+	let array2Item = array2[0]
+	let i = 1
+	let j = 1
+
+	// Check input
+	if (array1.length === 0) {
+		return array2
+	}
+	if (array2.length === 0) {
+		return array1
+	}
+
+	while (array1Item || array2Item) {
+		if (!array2Item || array1Item < array2Item) {
+			mergedArray.push(array1Item)
+			array1Item = array1[i]
+			i++
+		} else {
+			mergedArray.push(array2Item)
+			array2Item = array2[j]
+			j++
+		}
+	}
+
+	return mergedArray
+}
+
+mergeSortedArrays([0, 3, 4], [4, 6, 30])
